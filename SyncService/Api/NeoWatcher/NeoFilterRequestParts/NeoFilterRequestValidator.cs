9d97c072df0d31705779530fed2fc570c61c356a
AlyoshkinDV <alyoshkin.dmitry@yandex.ru>
2025-09-16 14:42:26 +0300

    правки в валидаторе

--- SyncService/Api/NeoWatcher/NeoFilterRequestParts/NeoFilterRequestValidator.cs
+++ SyncService/Api/NeoWatcher/NeoFilterRequestParts/NeoFilterRequestValidator.cs
@@ -5,13 +5,13 @@ public static class NeoFilterRequestValidator
     public static void Validate(NeoFilterRequest request)
     {
         if (request is { From: not null, To: not null }
-            && request.From <= request.To)
+            && request.From >= request.To)
             throw new ArgumentException("From must be less than To");
         
         if (request.MaxDiameter is <= 0)
             throw new ArgumentException("MaxDiameter must be greater than 0");
         
         if (request.MinDiameter is < 0)
-            throw new ArgumentException("MinDiameter must be greater than 0");
+            throw new ArgumentException("MinDiameter should not be a negative ");
     }
 }
